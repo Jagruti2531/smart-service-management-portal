7476a4142c66b3a476e0d1fe4245968173f284b7
JagrutiSalunkhe25 <jagrutisalunkhe143@gmail.com>
2026-09-09 19:55:51 +0530

    Remove build files and add gitignore

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(name,email,password,role) VALUES
-('System Admin','admin@smartservice.com','{noop}admin123','ADMIN'),
-('Rahul Sharma','employee@smartservice.com','{noop}employee123','EMPLOYEE'),
-('Jagruti Salunkhe','customer@smartservice.com','{noop}customer123','CUSTOMER');
-INSERT INTO tickets(ticket_number,title,description,category,priority,status,customer_name,customer_email,assigned_employee,created_at,updated_at) VALUES
-('TKT-1001','Unable to login','Customer cannot access the portal.','Technical','HIGH','ASSIGNED','Jagruti Salunkhe','customer@smartservice.com','employee@smartservice.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-('TKT-1002','Payment verification issue','Payment succeeded but order is pending.','Payment','CRITICAL','IN_PROGRESS','Jagruti Salunkhe','customer@smartservice.com','employee@smartservice.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-('TKT-1003','Update customer profile','Customer requested profile update.','Account','LOW','RESOLVED','Jagruti Salunkhe','customer@smartservice.com','employee@smartservice.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
